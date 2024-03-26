c0f81ec6ba4ab23c0d8c1495d484aaa418f2507d
matthewdegregorio <mdegregorio01@gmail.com>
2024-03-26 14:00:02 -0500

    add example raise test and example for multiple elements in parameters

--- tests/cms/content/test_elements.py
+++ tests/cms/content/test_elements.py
@@ -4,16 +4,30 @@ from bakedpytato.cms.content import _elements
 
 
 @pytest.mark.parametrize(
-    "tag",
+    "tag, my_other_val",
     [
-        'html',
-        'div',
-        'a',
-        'img',
-        'body',
-        'p'
+        ('html', 1),
+        ('div', 2),
+        ('a', 3),
+        ('img', 4),
+        ('body', 5),
+        ('p', 6)
     ]
 )
-def test_get_tags(tag):
+def test_get_tags(tag, my_other_val):
+    # TODO: remove example second value here; for demonstration only
+    print(my_other_val)
     result = _elements._get_tag(tag)
     assert issubclass(result, _elements.tags.html_tag)
+
+
+@pytest.mark.parametrize(
+    "tag",
+    [
+        'womp',
+        'derp'
+    ]
+)
+def test_raises_on_invalid_tag(tag):
+    with pytest.raises(AttributeError):
+        _elements._get_tag(tag)
